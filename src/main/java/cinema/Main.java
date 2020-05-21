dedc5eaef0efa958c75d284de904fd253bcb2ed7
Vitalii <59371243+VitaliiLeshchenko@users.noreply.github.com>
2020-05-21 11:29:22 +0300

    Hw 26 (#2)
    * added Injector, Logger and annotation: Dao and Service
    
    * added Injector, Logger and annotation: Dao and Service
    
    * added solution.
    
    * fixed solution
    
    * added Injector, Logger and annotation: Dao and Service
    
    * changed DataProcessingException

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,7 +1,17 @@
 package cinema;
 
-public class Main {
-    public static void main(String[] args) {
+import cinema.model.Movie;
+import cinema.service.MovieService;
+import cinema.util.Injector;
 
+public class Main {
+    private static final Injector INJECTOR = Injector.getInstance("cinema");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Another title");
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
+        movieService.add(movie);
+        movieService.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/cinema/exception/DataProcessingException.java
@@ -0,0 +1,7 @@
+package cinema.exception;
+
+public class DataProcessingException extends RuntimeException {
+    public DataProcessingException(String message, Exception exception) {
+        super(message, exception);
+    }
+}
