6cd5b801ce75314b67b73c65c16367169064477f
VitaliiLeshchenko <411100253g@gmail.com>
2020-05-21 10:34:43 +0300

    added Injector, Logger and annotation: Dao and Service

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -5,14 +5,13 @@ import cinema.service.MovieService;
 import cinema.util.Injector;
 
 public class Main {
-    private static final Injector injector = Injector.getInstance("cinema");
+    private static final Injector INJECTOR = Injector.getInstance("cinema");
 
     public static void main(String[] args) {
         Movie movie = new Movie();
         movie.setTitle("Another title");
-        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        MovieService movieService = (MovieService) INJECTOR.getInstance(MovieService.class);
         movieService.add(movie);
-
         movieService.getAll().forEach(System.out::println);
     }
 }
