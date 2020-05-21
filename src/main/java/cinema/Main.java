5cb4dd498c54a08952fc24ba48a563f29c4ad8e8
VitaliiLeshchenko <411100253g@gmail.com>
2020-05-21 09:30:55 +0300

    added solution.

--- src/main/java/cinema/Main.java
+++ src/main/java/cinema/Main.java
@@ -1,7 +1,18 @@
 package cinema;
 
-public class Main {
-    public static void main(String[] args) {
+import cinema.model.Movie;
+import cinema.service.MovieService;
+import cinema.util.Injector;
 
+public class Main {
+    private static final Injector injector = Injector.getInstance("cinema");
+
+    public static void main(String[] args) {
+        Movie movie = new Movie();
+        movie.setTitle("Another title");
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
+        movieService.add(movie);
+
+        movieService.getAll().forEach(System.out::println);
     }
 }
---
+++ src/main/java/cinema/dao/impl/MovieDaoImpl.java
@@ -0,0 +1,42 @@
+package cinema.dao.impl;
+
+import cinema.dao.Dao;
+import cinema.dao.MovieDao;
+import cinema.model.Movie;
+import cinema.util.HibernateUtil;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class MovieDaoImpl implements MovieDao {
+    private static final Logger LOGGER = Logger.getLogger(MovieDaoImpl.class);
+
+    @Override
+    public Movie add(Movie movie) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            movie.setId((Long) session.save(movie));
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            LOGGER.error(e);
+        }
+        return movie;
+    }
+
+    @Override
+    public List<Movie> getAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            CriteriaQuery<Movie> criteriaQuery = session
+                    .getCriteriaBuilder().createQuery(Movie.class);
+            criteriaQuery.from(Movie.class);
+            return session.createQuery(criteriaQuery).getResultList();
+        }
+    }
+}
