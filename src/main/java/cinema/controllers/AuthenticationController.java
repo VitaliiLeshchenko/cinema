1bde229542b8f24e7616bf71c4f5f866f181d5de
VitaliiLeshchenko <411100253g@gmail.com>
2020-06-14 23:09:39 +0300

    added registration of new shopping cart for every user

--- src/main/java/cinema/controllers/AuthenticationController.java
+++ src/main/java/cinema/controllers/AuthenticationController.java
@@ -1,7 +1,10 @@
 package cinema.controllers;
 
+import cinema.model.User;
 import cinema.model.dto.UserRequestDto;
 import cinema.service.AuthenticationService;
+import cinema.service.ShoppingCartService;
+import cinema.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,8 +18,15 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ShoppingCartService shoppingCartService;
+
     @PostMapping
     public void registerUser(@RequestBody UserRequestDto dto) {
-        authenticationService.register(dto.getEmail(), dto.getPassword());
+        User user = authenticationService.register(dto.getEmail(), dto.getPassword());
+        shoppingCartService.registerNewShoppingCart(user);
     }
 }
